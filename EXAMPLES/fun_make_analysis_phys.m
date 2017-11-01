a6724493df3d0cccec5bd6fc475295cd7f512c36
Andy Ridgwell <derp@derpy.co.uk>
2017-11-01 14:36:30 -0700

    added draft examples plotting configs

---
+++ EXAMPLES/fun_make_analysis_phys.m
@@ -0,0 +1,26 @@
+function [] = fun_make_analysis_phys(DUM_EXP,DUM_T)
+
+% *** PHYSICS *********************************************************** %
+%
+% topo
+plot_fields_biogem_2d(DUM_EXP,'','grid_topo','',DUM_T,-1,16,'',-1.0,-6000.0,0.0,24,'','plot_fields_settings_GRID',[DUM_EXP '.GRID']);
+% seaice
+plot_fields_biogem_2d(DUM_EXP,'','phys_seaice','',DUM_T,-1,16,'',1.0,0.0,100.0,20,'','plot_fields_settings_MISC',[DUM_EXP '.SEAICE']);
+% temp
+plot_fields_biogem_3d_k(DUM_EXP,'','ocn_temp','',DUM_T,-1,16,'',1.0,0.0,40.0,40,'','plot_fields_settings_TEMP',[DUM_EXP '.SST']);
+% PSI
+plot_fields_biogem_2d(DUM_EXP,'','atm_temp','',DUM_T,-1,16,'',1.0,0.0,30.0,30,'','plot_fields_settings_PSI',[DUM_EXP '.PSI']);
+%%%% PSI (+ temp)
+%%%plot_fields_biogem_2d(DUM_EXP,'','ocn_sur_temp','',DUM_T,-1,16,'',1.0,0.0,40.0,40,'','plot_fields_settings_PSI_temp',[DUM_EXP '.PSI_temp']);
+% OPSI
+plot_fields_biogem_3d_i(DUM_EXP,'','ocn_temp','',DUM_T,-1,0,'',1.0,0.0,30.0,30,'','plot_fields_settings_OPSI',[DUM_EXP '.OPSI']);
+%%%% OPSI (+ temp)
+%%%plot_fields_biogem_3d_i(DUM_EXP,'','ocn_temp','',DUM_T,-1,0,'',1.0,0.0,30.0,30,'','plot_fields_settings_OPSI_temp',[DUM_EXP '.OPSI_temp']);
+%%%% wind stress
+%%%plot_fields_biogem_2d(DUM_EXP,'','ocn_sur_temp','',9999.5,-1,16,'',1.0,0.0,30.0,30,'','plot_fields_settings_tau',[DUM_EXP '.tau']);
+% currents (+ speed)
+plot_fields_biogem_3d_k(DUM_EXP,'','ocn_temp','',DUM_T,-1,16,'',1.0,0.0,0.1,20,'','plot_fields_settings_UV',[DUM_EXP '.uv']);
+% currents (+ temp)
+plot_fields_biogem_3d_k(DUM_EXP,'','ocn_temp','',DUM_T,-1,16,'',1.0,0.0,40.0,40,'','plot_fields_settings_UV_temp',[DUM_EXP '.uv_temp']);
+%
+% *********************************************************************** %
---
+++ EXAMPLES/make_analysis_ALL.m
@@ -0,0 +1,33 @@
+
+% CTRL
+fun_make_analysis_phys('170805.FM450a.CTRL',4999.5);
+fun_make_analysis_bio('170805.FM450a.CTRL',4999.5);
+close all;
+
+% modernS0
+fun_make_analysis_phys('170805.FM450a.SPIN_modernS0',4999.5);
+%fun_make_analysis_bio('170805.FM450a.SPIN_modernS0',4999.5);
+close all;
+
+% modernpO2
+%fun_make_analysis_phys('170805.FM450a.SPIN_modernpO2',4999.5);
+fun_make_analysis_bio('170805.FM450a.SPIN_modernpO2',4999.5);
+close all;
+
+% midpO2
+%fun_make_analysis_phys('170805.FM450a.SPIN_midpO2',4999.5);
+fun_make_analysis_bio('170805.FM450a.SPIN_midpO2',4999.5);
+close all;
+
+% lowpO2
+%fun_make_analysis_phys('170805.FM450a.SPIN_lowpO2',4999.5);
+fun_make_analysis_bio('170805.FM450a.SPIN_lowpO2',4999.5);
+close all;
+
+% x2PO4
+%fun_make_analysis_phys('170805.FM450a.SPIN_x2PO4',4999.5);
+fun_make_analysis_bio('170805.FM450a.SPIN_x2PO4',4999.5);
+close all;
+
+
+
