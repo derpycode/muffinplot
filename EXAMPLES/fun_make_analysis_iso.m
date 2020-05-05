48a5dee857df32e3e09b418b5de37ffd9e67fe1a
Andy Ridgwell <derp@derpy.co.uk>
2020-05-05 10:54:30 -0700

    -

---
+++ EXAMPLES/fun_make_analysis_iso.m
@@ -0,0 +1,27 @@
+function [] = fun_make_analysis_iso(DUM_EXP,DUM_T,DUM_PAR)
+
+% *** maximum value surface ********************************************* %
+%
+% variable: DUM_PAR
+% NOTE: auto-scale
+plot_fields_biogem_3d_k(DUM_EXP,'',DUM_PAR,'',DUM_T,-1,0,'',0.0,0.0,1.0,20,'','plot_fields_settings_MAX',[DUM_EXP '.MAX_' DUM_PAR]);
+% [SURFACE DEPTH]
+% %%%
+%
+% *** minimum value surface ********************************************* %
+%
+% variable: DUM_PAR
+% NOTE: auto-scale
+plot_fields_biogem_3d_k(DUM_EXP,'',DUM_PAR,'',DUM_T,-1,0,'',0.0,0.0,1.0,20,'','plot_fields_settings_MIN',[DUM_EXP '.MIN_' DUM_PAR]);
+% [SURFACE DEPTH]
+% %%%
+%
+% *** water columdn mean ************************************************ %
+%
+% variable: DUM_PAR
+% NOTE: auto-scale
+plot_fields_biogem_3d_k(DUM_EXP,'',DUM_PAR,'',DUM_T,-1,0,'',0.0,0.0,1.0,20,'','plot_fields_settings_AVE',[DUM_EXP '.AVE_' DUM_PAR]);
+%
+% *********************************************************************** %
+
+close all;
