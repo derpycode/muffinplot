e8ff69cf74c93a3d2d165184c6f0d14b03ff1077
Andy Ridgwell <derp@derpy.co.uk>
2021-02-25 08:17:38 -0800

    %   21/02/25: added more fileformat flexibility and filtering

--- source/fun_read_file.m
+++ source/fun_read_file.m
@@ -13,6 +13,7 @@ function [OUTPUT] = fun_read_file(PFILENAME)
 %   ***********************************************************************
 %
 %   20/12/29: CREATED
+%   21/02/25: added more fileformat flexibility and filtering
 %
 %   ***********************************************************************
 
@@ -70,16 +71,28 @@ end
 % create vector for storing comment line numbers
 v_comment = [];
 % look for comment lines (lines starting with '%')
+% NOTE: test whether the '%' occurs at position 1 ...
+%       (could also allow for it to be further along)
 for n = 1:n_lines
     loc_comment = strfind(loc_file{n},'%');
     if ~isempty(loc_comment)
-        v_comment = [v_comment n];     
+        if (loc_comment(1) == 1)
+            v_comment = [v_comment n]; 
+        end
     end    
 end
 % remove comment lines
 if ~isempty(v_comment)
-    disp([' * ACTION:  Marking comment lines: #' num2str(v_comment) ' for removal.']);
+    disp([' * ACTION:  Marking comment lines #: ' num2str(v_comment) ' for removal.']);
     loc_file(v_comment,:) = [];
+end
+% update number of lines
+n_lines = length(loc_file);
+% remove any blank line at end
+n = n_lines;
+loc_line = loc_file{n};
+if (isempty(loc_line))
+    loc_file(n,:) = [];
 end
 % update number of lines
 n_lines = length(loc_file);
@@ -87,6 +100,15 @@ n_lines = length(loc_file);
 % *** parse delimanators ************************************************ %
 %
 % replace all deliminator characters by a single space
+% NOTE:
+% 009 == TAB
+% 032 == SPACE
+% 035 == #
+% 036 == $
+% 037 == %
+% 038 == &
+% 044 == ,
+% loop through all lines of text
 for n = 1:n_lines
     % determine format from first line
     loc_line = loc_file{n};
@@ -94,6 +116,16 @@ for n = 1:n_lines
     loc_line = strrep(loc_line,char(9),char(32));
     % replace any commas with spaces
     loc_line = strrep(loc_line,char(44),char(32));
+    % replace any single/double like quotation marks with spaces
+    loc_line = strrep(loc_line,char(34),char(32));
+    loc_line = strrep(loc_line,char(39),char(32));
+    loc_line = strrep(loc_line,char(96),char(32));
+    loc_line = strrep(loc_line,char(145),char(32));
+    loc_line = strrep(loc_line,char(146),char(32));
+    loc_line = strrep(loc_line,char(147),char(32));
+    loc_line = strrep(loc_line,char(148),char(32));
+    % replace any % characters with spaces
+    loc_line = strrep(loc_line,char(37),char(32));
     % now reduce any multiple spaces to a single space
     delimreplace=true;
     while (delimreplace == true)
@@ -104,6 +136,10 @@ for n = 1:n_lines
             delimreplace=false;
         end
     end
+    % remove any spaces at the very start and end of the lines
+    loc_spaces = strfind(loc_line,char(32));
+    if (loc_spaces(end) == length(loc_line)), loc_line(end) = []; end
+    if (loc_spaces(1) == 1), loc_line(1) = []; end
     % replace entry
     loc_file{n} = loc_line;
 end
@@ -113,9 +149,11 @@ end
 % work on the basis that the first line of data is 'correct'
 loc_line = loc_file{1};
 % deduce number of columns from first line
+% (remember that colums == seperators + 1)
 if (numel(strfind(loc_line,char(32))) > 0)
-    n_space   = numel(strfind(loc_line,char(32)));
-    n_columns = n_space + 1;
+    loc_spaces = strfind(loc_line,char(32));
+    n_space    = numel(loc_spaces);
+    n_columns  = n_space + 1;
 else
     disp([' * ERROR:   Cannot determine data file format.']);
     disp([' * ACTION:  EXITING ...']);
@@ -132,15 +170,6 @@ for n = 1:n_columns
 end
 %
 % *** filter out lines that do not conform to the first ***************** %
-%
-% (0) BLANK LINE AT END
-n = n_lines;
-loc_line  = loc_file{n};
-if (isempty(loc_line))
-    loc_file(n,:) = [];
-end
-% update number of lines
-n_lines = length(loc_file);
 %
 % (1) MISSING ENTRIES
 % create vector for storing non-confirming lines
