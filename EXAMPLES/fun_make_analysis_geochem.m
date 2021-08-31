6462e19e9cc7f7a0d07242b4a8c7c3fd588ca8e1
Andy Ridgwell <derp@derpy.co.uk>
2021-08-31 10:25:06 -0700

    -

--- EXAMPLES/fun_make_analysis_geochem.m
+++ EXAMPLES/fun_make_analysis_geochem.m
@@ -13,9 +13,9 @@ plot_fields_biogem_3d_k(DUM_EXP,'','ocn_O2','',DUM_T,-1,16,'',1.0E-6,0.0,300.0,3
 % *** GEOCHEM -- benthic ************************************************ %
 %
 % [PO4]
-plot_fields_biogem_3d_k(DUM_EXP,'','ocn_PO4','',DUM_T,-1,16,'',1.0E-6,0.0,3.0,30,'','plot_fields_SETTINGS_GEOCHEM',[DUM_NAME '.GEOCHEM_BENpo4']);
+plot_fields_biogem_3d_k(DUM_EXP,'','ocn_PO4','',DUM_T,-1,-1,'',1.0E-6,0.0,3.0,30,'','plot_fields_SETTINGS_GEOCHEM',[DUM_NAME '.GEOCHEM_BENpo4']);
 % [O2]
-plot_fields_biogem_3d_k(DUM_EXP,'','ocn_O2','',DUM_T,-1,16,'',1.0E-6,0.0,300.0,30,'','plot_fields_SETTINGS_GEOCHEM',[DUM_NAME '.GEOCHEM_BENo2']);
+plot_fields_biogem_3d_k(DUM_EXP,'','ocn_O2','',DUM_T,-1,-1,'',1.0E-6,0.0,300.0,30,'','plot_fields_SETTINGS_GEOCHEM',[DUM_NAME '.GEOCHEM_BENo2']);
 %
 % *** GEOCHEM -- zonal (global) ***************************************** %
 %
